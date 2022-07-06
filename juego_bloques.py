1ed16c17831b371cbe1d79e1c576f1e68ae969f6
Isaías Bonilla <descarteskant15@gmail.com>
2022-07-06 18:07:16 -0500

    Agregando funciion de cerrar ventana con X

--- juego_bloques.py
+++ juego_bloques.py
@@ -1,5 +1,6 @@
 from curses.ascii import alt
 from pickle import TRUE
+import sys
 import pygame as pg
 
 ancho = 640
@@ -7,4 +8,7 @@ alto = 480
 pantalla = pg.display.set_mode((ancho,alto))
 
 while TRUE:
+    for eveto in pg.event.get():
+        if eveto.type == pg.QUIT:
+            sys.exit
     pg.display.flip()
