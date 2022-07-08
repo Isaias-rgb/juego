6c5932efe468bfa8d80d720771ceb945f98fdae8
Isaías Bonilla <descarteskant15@gmail.com>
2022-07-08 13:03:21 -0500

    Agregando los bloques del nivel

--- nivel1.py
+++ nivel1.py
@@ -1,5 +1,24 @@
-import os
-os.path.abspath(os.getcwd())
+import pygame as pg
+from soupsieve import select
+class Ladrillo(pg.sprite.Sprite):
+    def __init__(self,posicion) -> None:
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load('imagenes/ladrillo.png')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = posicion
 
-r = os.path.dirname(os.path.realpath(__file__))
-print(r)
+class Muro(pg.sprite.Group):
+    def __init__(self,cantidad_ladrillos,ancho) -> None:
+        pg.sprite.Group.__init__(self)
+
+        pos_x = 0
+        pos_y = 30
+        for i in range(cantidad_ladrillos):
+            ladrillo = Ladrillo((pos_x,pos_y))
+            self.add(ladrillo)
+
+            pos_x += ladrillo.rect.width         
+
+            if pos_x >= ancho:
+                pos_x = 0
+                pos_y += ladrillo.rect.height
