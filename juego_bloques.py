75ed5b4a0b99ec52b441f60ba2e5296879c8d928
Isaías Bonilla <descarteskant15@gmail.com>
2022-07-08 13:24:00 -0500

    Agregando colision de bolita con ladrillos y destriccion de ladrillos

--- juego_bloques.py
+++ juego_bloques.py
@@ -38,17 +38,33 @@ while True:
     
     #Actualiza la posicion de la bolita 
     bolita.update()
+    
     #Colision entre bola y paleta
     if pg.sprite.collide_rect(bolita,jugador):
         bolita.speed[1] = -bolita.speed[1]   
+    #Colisicion de la pelota con ladrillos
+    lista = pg.sprite.spritecollide(bolita, muro, False)
+    if lista:
+        ladrillo = lista[0]
+        cx = bolita.rect.centerx #obtiene la pos en x
+        if cx < ladrillo.rect.left or cx > ladrillo.rect.right:
+            bolita.speed[0] = -bolita.speed[0]
+        else:
+            bolita.speed[1] = -bolita.speed[1]
+        muro.remove(ladrillo)
+                
     #Rellenamos la pantalla
     pantalla.fill(color_fondo)           
+    
     #dibuja la bolita en la pantalla
     pantalla.blit(bolita.image,bolita.rect)
+    
     #Dibuja paleta
     pantalla.blit(jugador.image,jugador.rect)
+    
     #Dibuja el muro
     muro.draw(pantalla) 
+    
     #Actualiza los objetos en la pantalla
     pg.display.flip()
 
